4cf44e236d47651f7239ade6508281c7a76cc6eb
srn73 <133917170+srn73@users.noreply.github.com>
2025-08-15 21:45:31 -0400

    add privacy policy for srn feud

---
+++ src/routes/policies/srn-feud/+page.svelte
@@ -0,0 +1,14 @@
+<title>Privacy Policy - SRN Feud</title>
+
+<main class="p-4 d-flex flex-column justify-content-center align-items-center text-center vh-100">
+    <h1>Privacy Policy</h1>
+    <h5>Last updated: 8/15/2025</h5>
+
+    <p>
+        The extension "SRN Feud" does not collect, transmit, or store any personally identifiable information
+        about users on any external servers. Any data associated with the extension (e.g. preferences) is only
+        stored and processed on your device and never shared with the developer ("srn73") or any third parties.
+    </p>
+
+    <p>If you have any questions, please contact me at <a href="mailto:contact@srn73.com">contact@srn73.com</a>.</p>
+</main>
